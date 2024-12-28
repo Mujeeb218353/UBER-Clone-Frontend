2a68ccaeb70c0821a60c3c828fe9ee54b1fd16ac
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-12-28 12:00:00 +0500

    Added ForgotPassword.jsx

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgotPassword = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center'>ForgotPassword</div>
+  )
+}
+
+export default ForgotPassword
